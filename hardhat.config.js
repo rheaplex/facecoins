ebd4e1461e295aed8bf16bc80fa07d80486e5aa3
Rhea Myers <rhea@myers.studio>
2022-10-23 15:37:06 -0700

    Modernize environment a little.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.17",
 };
